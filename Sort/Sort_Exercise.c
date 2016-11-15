8b8f4b437a6adbe00be7e9253e755193124b144b
Changho Kang <inodient@gmail.com>
2016-11-15 21:02:39 +0900

    Quick Sort Error Case

--- Sort/Sort_Exercise.c
+++ Sort/Sort_Exercise.c
@@ -1,3 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Swap( int* A, int* B ){
+	int Temp = *A;
+	*A = *B;
+	*B = Temp;
+}
+
+void BubbleSort( int DataSet[], int Length ){
+	int i = 0, j = 0;
+
+	for( i=0; i<Length-1; i++ ){
+		for( j=0; j<Length-(i+1); j++ ){
+			if( DataSet[j] > DataSet[j+1] ){
+				Swap( &DataSet[j], &DataSet[j+1] );
+			}
+		}
+	}
+}
+void InsertionSort( int DataSet[], int Length ){
+	int i = 0, j = 0, Value = 0;
+
+	for( i=1; i<Length; i++ ){
+		if( DataSet[i-1] < DataSet[i] )
+			continue;
+
+		Value = DataSet[i];
+
+		for( j=0; j<i; j++ ){
+			if( DataSet[j] > Value ){
+				memmove( &DataSet[j+1], &DataSet[j], sizeof( DataSet[0] ) * (i-j) );
+				DataSet[j] = Value;
+				break;
+			}
+		}
+	}
+}
+
+int Partition( int DataSet[], int Left, int Right ){
+	int First = Left;
+	int Pivot = DataSet[First];
+
+	++Left;
+
+	while( Left <= Right ){
+		while( DataSet[Left] <= Pivot && Left < Right ){
+			++Left;
+		}
+		while( DataSet[Right] > Pivot && Left <= Right ){
+			--Right;
+		}
+
+		if( Left < Right ){
+			Swap( &DataSet[Left], &DataSet[Right] );
+		} else{
+			break;
+		}
+	}
+
+	Swap( &DataSet[First], &DataSet[Right] );
+
+	return Right;
+}
+
+void QuickSort( int DataSet[], int Left, int Right ){
+	if( Left < Right ){
+		int Index = Partition( DataSet, Left, Right );
+
+		QuickSort( DataSet, Left, Index - 1 );
+		QuickSort( DataSet, Index + 1, Right );
+	}
+}
+
+
+
+void Merge( int DataSet[], int StartIndex, int MiddleIndex, int EndIndex ){
+	int i;
+	int LeftIndex = StartIndex;
+	int RightIndex = MiddleIndex + 1;
+	int DestIndex = 0;
+
+	int* Destination = (int*)malloc( sizeof(int) * (EndIndex - StartIndex + 1) );
+
+	while( LeftIndex <= MiddleIndex && RightIndex <= EndIndex ){
+		if( DataSet[LeftIndex] < DataSet[RightIndex] ){
+			Destination[DestIndex] = DataSet[LeftIndex];
+			LeftIndex++;
+		} else {
+			Destination[DestIndex] = DataSet[RightIndex];
+			RightIndex++;
+		}
+		DestIndex++;
+	}
+
+	while( LeftIndex <= MiddleIndex ){
+		Destination[DestIndex++] = DataSet[LeftIndex++];
+	}
+	while( RightIndex <= EndIndex ){
+		Destination[DestIndex++] = DataSet[RightIndex++];
+	}
+
+	DestIndex = 0;
+	for( i=StartIndex; i<=EndIndex; i++ ){
+		DataSet[i] = Destination[DestIndex++];
+	}
+
+	free( Destination );
+}
+
+void MergeSort( int DataSet[], int StartIndex, int EndIndex ){
+	int MiddleIndex;
+
+	if( EndIndex - StartIndex < 1 ){
+		return;
+	}
+
+	MiddleIndex = ( StartIndex + EndIndex ) / 2 ;
+
+	MergeSort( DataSet, StartIndex, MiddleIndex );
+	MergeSort( DataSet, MiddleIndex + 1, EndIndex );
+
+	Merge( DataSet, StartIndex, MiddleIndex, EndIndex );
+}
+
+int main( void ){
+
+	int DataSet[] = {0,2,3,4,5,1,6,3,5,3,2,4,6,7,8,4,3,2,1,4,5,6,8,4,3,7,3,4,5,3,2};
+	int Length = sizeof DataSet / sizeof DataSet[0];
+
+	BubbleSort( DataSet, Length );
+	for( int i=0; i<Length; i++ ){
+		printf( "%d ", DataSet[i] );
+	}
+	printf( "\n" );
+
+	int DataSet2[] = {0,2,3,4,5,1,6,3,5,3,2,4,6,7,8,4,3,2,1,4,5,6,8,4,3,7,3,4,5,3,2};
+	Length = sizeof DataSet2 / sizeof DataSet2[0];
+
+	InsertionSort( DataSet2, Length );
+	for( int i=0; i<Length; i++ ){
+		printf( "%d ", DataSet2[i] );
+	}
+	printf( "\n" );
+
+	int DataSet3[] = {0,2,3,4,5,1,6,3,5,3,2,4,6,7,8,4,3,2,1,4,5,6,8,4,3,7,3,4,5,3,2};
+	Length = sizeof DataSet3 / sizeof DataSet3[0];
+
+	QuickSort( DataSet3, 0, Length-1 );
+	for( int i=0; i<Length; i++ ){
+		printf( "%d ", DataSet3[i] );
+	}
+	printf( "\n" );
+
+	int DataSet4[] = {0,2,3,4,5,1,6,3,5,3,2,4,6,7,8,4,3,2,1,4,5,6,8,4,3,7,3,4,5,3,2};
+	Length = sizeof DataSet4 / sizeof DataSet4[0];
+
+	MergeSort( DataSet4, 0, Length-1 );
+	for( int i=0; i<Length; i++ ){
+		printf( "%d ", DataSet4[i] );
+	}
+	printf( "\n" );
+
+
+	return 0;
+}
+//
 //#include <stdio.h>
 //#include <stdlib.h>
 //#include <string.h>
@@ -8,11 +176,9 @@
 //	*B = Temp;
 //}
 //
-//void BubbleSort( int DataSet[], int Length ){
-//	int i, j =0;
-//
-//	for( i=0; i<Length-1; i++ ){
-//		for( j=0; j<Length-(i+1); j++ ){
+//int BubbleSort( int DataSet[], int Length ){
+//	for( int i=0; i<Length-1; i++ ){
+//		for( int j=0; j<Length-(i+1); j++ ){
 //			if( DataSet[j] > DataSet[j+1] ){
 //				Swap( &DataSet[j], &DataSet[j+1] );
 //			}
@@ -20,20 +186,19 @@
 //	}
 //}
 //
-//void InsertionSort( int DataSet[], int Length ){
-//	int i = 0;
-//	int j = 0;
+//int InsertionSort( int DataSet[], int Length ){
 //	int Value = 0;
 //
-//	for( i=1; i<Length; i++ ){
-//		if( DataSet[i-1] < DataSet[i] )
+//	for( int i=1; i<Length; i++ ){
+//		if( DataSet[i-1] < DataSet[i] ){
 //			continue;
+//		}
 //
 //		Value = DataSet[i];
 //
-//		for( j=0; j<i; j++ ){
+//		for( int j=0; j<i; j++ ){
 //			if( DataSet[j] > Value ){
-//				memmove( &DataSet[j+1], &DataSet[j], sizeof( DataSet[0] ) * ( i - j ) );
+//				memmove( &DataSet[j+1], &DataSet[j], sizeof( DataSet[0] ) * (i-j) );
 //				DataSet[j] = Value;
 //				break;
 //			}
@@ -41,44 +206,16 @@
 //	}
 //}
 //
-//int Partition_Pivot_Left( int DataSet[], int Left, int Right ){
-//	int First = Left;
-//	int Pivot = DataSet[Left];
-//
-//	++Left;
-//
-//	while( Left <= Right ){
-//		while( DataSet[Left] <= Pivot && Left < Right ){
-//			++Left;
-//		}
-//		while( DataSet[Right] > Pivot && Left <= Right ){
-//			--Right;
-//		}
-//
-//		if( Left < Right ){
-//			Swap( &DataSet[Left], &DataSet[Right] );
-//		} else{
-//			break;
-//		}
-//	}
-//
-//	Swap( &DataSet[Right], &DataSet[First] );
-//
-//	return Right;
-//}
-//
-//int Partition_Pivot_Right( int DataSet[], int Left, int Right ){
+//int Partition( int DataSet[], int Left, int Right ){
 //	int Last = Right;
-//	int Pivot = DataSet[Right];
-//
-//	--Right;
+//	int Pivot = DataSet[Last];
 //
 //	while( Left <= Right ){
-//		while( DataSet[Right] >= Pivot && Left < Right ){
-//			--Right;
-//		}
-//		while( DataSet[Left] < Pivot && Left <= Right ){
+//		while( DataSet[Left] <= Pivot && Left <= Right ){
 //			++Left;
+//		}
+//		while( DataSet[Right] > Pivot && Left < Right ){
+//			--Right;
 //		}
 //
 //		if( Left < Right ){
@@ -93,65 +230,25 @@
 //	return Left;
 //}
 //
-//void QuickSort_Pivot_Left( int DataSet[], int Left, int Right ){
+//void QuickSort( int DataSet[], int Left, int Right ){
 //	if( Left < Right ){
-//		int Index = Partition_Pivot_Left( DataSet, Left, Right );
+//		int Index = Partition( DataSet, Left, Right );
 //
-//		QuickSort_Pivot_Left( DataSet, Left, Index - 1 );
-//		QuickSort_Pivot_Left( DataSet, Index + 1, Right );
-//	}
-//}
-//
-//void QuickSort_Pivot_Right( int DataSet[], int Left, int Right ){
-//	if( Left < Right ){
-//		int Index = Partition_Pivot_Right( DataSet, Left, Right );
-//
-//		QuickSort_Pivot_Right( DataSet, Left, Index - 1 );
-//		QuickSort_Pivot_Right( DataSet, Index + 1, Right );
+//		QuickSort( DataSet, Left, Index - 1 );
+//		QuickSort( DataSet, Index + 1, Right );
 //	}
 //}
 //
 //int main( void ){
-//	int i = 0;
-//	int DataSet[] = { 8,3,3,2,1,3,5,6,7,8,4,3,8,9,10 };
+//	int DataSet[] = {1,2,3,5,6,7,9,8,7,6,4,0,8,7,6,1,2,0,3,8,7,4,5,1,0,2,7,6,3,5,9,7,4,6,5,2,1,8,0,7,2,6,3,0,8,1,7,2,4,9,7,6,5,1,2,8,6,3,5,1,4,8,2,5,6,3,4,1,9,7,2,6,3,5,1,9,2,7,5,1,1,9,8,2,7,3,6,4};
 //	int Length = sizeof DataSet / sizeof DataSet[0];
 //
-//	QuickSort_Pivot_Left( DataSet, 0, Length - 1 );
+//	QuickSort( DataSet, 0, Length-1 );
 //
-//	printf( "Quick Sort : Pivot_Left\n" );
-//	for( i=0; i<Length; i++ ){
-//		printf( "%d	", DataSet[i] );
+//	for( int i=0; i<Length; i++ ){
+//		printf( "%d ", DataSet[i] );
 //	}
-//	printf( "\n" );
 //
-//	int DataSet2[] = { 11,5,2,4,3,1 };
-//	Length = sizeof DataSet2 / sizeof DataSet2[0];
-//
-//	QuickSort_Pivot_Right( DataSet2, 0, Length - 1 );
-//
-//	printf( "Quick Sort : Pivot_Right\n" );
-//	for( i=0; i<Length; i++ ){
-//		printf( "%d	", DataSet2[i] );
-//	}
-//	printf( "\n" );
-//
-//	printf( "Bubble Sort\n" );
-//	int DataSet3[] = { 10,9,8,7,6,5,4,3,2,1 };
-//	Length = sizeof DataSet3 / sizeof DataSet[0];
-//
-//	BubbleSort( DataSet3, Length );
-//	for( i=0; i<Length; i++ ){
-//		printf( "%d	", DataSet3[i] );
-//	}
-//	printf( "\n" );
-//
-//	printf( "Insertion Sort\n" );
-//	int DataSet4[] = { 8,3,3,2,1,3,5,6,7,8,4,3,8,9,10 };
-//	Length = sizeof DataSet4 / sizeof DataSet4[0];
-//
-//	InsertionSort( DataSet4, Length );
-//	for( i=0; i<Length; i++ ){
-//		printf( "%d	", DataSet4[i] );
-//	}
-//	printf( "\n" );
+//	return 0;
 //}
+
